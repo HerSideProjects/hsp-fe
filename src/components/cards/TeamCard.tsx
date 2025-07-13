fdafb036ff442c2dfc8f9dd90011d0e8fb5e262d
nadriha <hafez.nadhira@gmail.com>
2025-07-13 19:26:51 +0700

    feat: add team member

--- src/components/cards/TeamCard.tsx
+++ src/components/cards/TeamCard.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import Image from 'next/image'
+import { twMerge } from 'tailwind-merge';
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
     name?: string;
     imagePath: string;
     alt: string;
     role: string;
-    cardColor?: "bg-[#1C1719]";
+    className?: string;
 }
 
-const TeamCard = ({name, imagePath, cardColor, alt, role }: Props) => {
+const TeamCard = ({name, imagePath, className, alt, role }: Props) => {
+    
   return (
-    <div className={`${cardColor} text-[#FEF6EB] min-w-[268px] h-[462px] flex flex-col rounded-[20px] pl-6 pt-6 overflow-hidden`}>
+    <div className={twMerge(
+        `text-[#FEF6EB] min-w-[268px] h-[462px] flex flex-col rounded-[20px] pl-6 pt-6 overflow-hidden`, className
+      )}>
         <div>
             <p className="font-lostaMasta text-[32px]">{name}</p>
             <p className="text-2xl pr-6">{role}</p>
