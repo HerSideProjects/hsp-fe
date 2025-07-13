c60a34011f07625155daf48155ebf810dc34ec2c
nadriha <hafez.nadhira@gmail.com>
2025-07-13 19:16:38 +0700

    feat: add team member

--- src/components/cards/TeamCard.tsx
+++ src/components/cards/TeamCard.tsx
@@ -6,14 +6,15 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
     imagePath: string;
     alt: string;
     role: string;
+    cardColor?: "bg-[#1C1719]";
 }
 
-const TeamCard = ({name, imagePath, alt, role }: Props) => {
+const TeamCard = ({name, imagePath, cardColor, alt, role }: Props) => {
   return (
-    <div className="bg-[#1C1719] text-[#FEF6EB] min-w-[268px] h-[462px] flex flex-col rounded-[20px] pl-6 pt-6 overflow-hidden">
+    <div className={`${cardColor} text-[#FEF6EB] min-w-[268px] h-[462px] flex flex-col rounded-[20px] pl-6 pt-6 overflow-hidden`}>
         <div>
             <p className="font-lostaMasta text-[32px]">{name}</p>
-            <p className="text-2xl">{role}</p>
+            <p className="text-2xl pr-6">{role}</p>
         </div>
         <div className="flex-grow"></div>
         <div className="w-full flex justify-center">
