25db9e6eea44893278d5912a75c97f7c0e3d2f74
nadriha <hafez.nadhira@gmail.com>
2025-07-14 15:24:59 +0700

    feat: modal

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -11,6 +11,9 @@ import { RingIcon } from '../../../public/assets/icons/RingIcon'
 import { UpperBorder } from '../../../public/assets/icons/UpperBorder'
 import { toast } from 'react-hot-toast'
 import { SelectField } from '@/components/inputs/SelectField'
+import DetailEventSection from '@/components/modules/detailEventSection'
+import { Modal } from '@/components/modals/Modal'
+import { RegisterSuccess } from '../../../public/assets/icons/RegisterSuccess'
 
 const RegisterPage = () => {
   const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
@@ -19,6 +22,7 @@ const RegisterPage = () => {
   const [filePaymentProof, setFilePaymentProof] = useState<File | null>(null);
   const formRef = useRef<HTMLDivElement>(null)
   const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   const handleOnChangeInterest = (value: string[]) => {
     setSelectedCategory(value);
@@ -26,7 +30,10 @@ const RegisterPage = () => {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    toast.loading('Submitting registration...', {id : "register"});
+    toast.loading('Submitting registration...', {
+      id: "register",
+      duration: Infinity,
+    });
     setLoading(true);
     const form = e.currentTarget as HTMLFormElement;
     const formData = new FormData(form);
@@ -34,13 +41,14 @@ const RegisterPage = () => {
       formData.append("student_file", fileStudentProof);
     }
     if (filePaymentProof) {
-      formData.append("file", filePaymentProof); // default key `file` tetap dipakai kalau backend-nya belum diubah
+      formData.append("file", filePaymentProof);
     }
 
     const response = await submitRegistration(null, formData);
     if (response.success) {
       setLoading(false);
       toast.success(response.message, {id : "register"});
+      setShowModal(true);
       form.reset(); 
       setSelectedCategory([]);
       setFileStudentProof(null);
@@ -64,62 +72,7 @@ const RegisterPage = () => {
 
   return (
     <div>
-      <div className="bg-[#FEF6EB] min-h-screen md:pt-36 pt-32 md:px-16 px-8 flex flex-col gap-12 pb-40 md:pb-20 scroll-hidden">
-        <div className="flex min-[1070px]:flex-row flex-col md:gap-12">
-          <div className="flex flex-col gap-4">
-            <div className="flex md:flex-row flex-col md:items-center md:gap-6">
-              <div>
-                <RingIcon/>
-              </div>
-              <div className="flex flex-col text-[56px] md:text-6xl font-lostaMasta">
-                <p className="text-[#1C1719]">About</p>
-                <p className="text-[#EF3187] md:-mt-0 -mt-3 -mb-3 md:-mb-0">The Event</p>
-              </div>
-            </div>
-            <div>
-              <p className="md:text-2xl text-xl max-w-5/6">We&apos;re kicking things off with a flower pipecleaner workshop – a refreshing escape where you can get creative with your hands. Beyond the crafting, it&apos;s also a chance to connect with other women who share your interests.</p>
-            </div>
-            <div className="flex md:flex-row flex-col gap-4">
-              <div className="bg-[#E2F1AE] rounded-3xl p-4 w-fit">
-                <p className="md:text-2xl text-xl">🗓️ Saturday, 2 August 2025</p>
-              </div>
-              <div className="bg-[#E2F1AE] rounded-3xl p-4 w-fit">
-                <p className="md:text-2xl text-xl">📍Kopi Nako Depok</p>
-              </div>
-            </div>
-          </div>
-          <div className="relative md:min-w-[500px] h-[350px]">
-            <Image
-              src="/assets/images/bersama-details.png"
-              alt="foto bersama"
-              fill
-              className="object-contain"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col gap-4">
-          <p className="font-lostaMasta text-[32px]">Benefits You&apos;ll Love:</p>
-          <div className="flex md:flex-row flex-col gap-6">
-            <div className="bg-[#EF3187] rounded-[20px] p-6 text-[#FEF6EB] flex flex-col gap-1">
-              <p className="text-2xl font-lostaMasta">Hands-On Creative Escape</p>
-              <p>Refresh your mind as our dedicated mentor guides you through crafting charming flower pipe cleaners.</p>
-            </div>
-            <div className="bg-[#EF3187] rounded-[20px] p-6 text-[#FEF6EB] flex flex-col gap-1">
-              <p className="text-2xl font-lostaMasta">Connect with STEM Stars</p>
-              <p>Open conversations with inspiring mentors from diverse STEM fields. Ask your questions, share your journey, and gain insights</p>
-            </div>
-            <div className="bg-[#EF3187] rounded-[20px] p-6 text-[#FEF6EB] flex flex-col gap-1">
-              <p className="text-2xl font-lostaMasta">Build Your Supportive Circle</p>
-              <p>Expand your network, share experiences, and find encouragement in a friendly, low-pressure environment.</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex md:justify-end justify-center">
-          <div onClick={scrollToForm} className="bg-[#1C1719] text-[#FEF6EB] rounded-[10px] px-6 py-5 w-fit cursor-pointer">
-            <p className="font-bold">Regist Now ↓</p>
-          </div>
-        </div>
-      </div>
+      <DetailEventSection scrollToForm={scrollToForm} />
       <div ref={formRef} className="min-h-screen bg-[#A6C0FF]" style={{ backgroundImage: "url('/assets/images/bg-regist.png')" }}>
         <UpperBorder/>
         <main className="py-28 md:w-1/2 mx-auto flex flex-col items-center md:px-0 px-3">
@@ -180,6 +133,17 @@ const RegisterPage = () => {
               <input type="hidden" name="major" value={selectedValue} />
           </form>
         </main>
+        {showModal && (
+          <Modal onClose={() => setShowModal(false)}>
+            <div className="flex flex-col items-center md:gap-10 gap-5 md:py-4 py-2">
+              <RegisterSuccess/>
+              <div className="flex flex-col items-center gap-2 text-center">
+                <p className="font-lostaMasta md:text-3xl text-xl">Registration Successful</p>
+                <p className="text-sm">We will contact you via WhatsApp for further information.</p>
+              </div>
+            </div>
+          </Modal>
+        )}
       </div>
     </div>
   )
---
+++ src/components/modals/Modal.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import React, { useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+import { ModalClosed } from '../../../public/assets/icons/ModalClosed';
+
+interface Props {
+  children: React.ReactNode;
+  className?: string;
+  onClose?: () => void;
+}
+
+export const Modal = ({children, className, onClose, ...props} : Props) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClose = () => {
+    setIsOpen(false);
+    if (onClose) {
+      onClose();
+    }
+  };
+
+  return (
+    <div onClick={handleClose} className={`fixed w-full h-screen z-[100] bg-black/40 top-0 left-0 flex justify-center items-center ${!isOpen && 'hidden'}`}>
+        <div onClick={e => e.stopPropagation()} className={twMerge("absolute bg-[#FEF6EB] rounded-2xl border border-[#EF3187] shadow-[0_0_15px_rgba(0,0,0,0.1)] w-fit flex flex-col md:py-5 md:px-5 px-3 py-3 max-w-5/6 md:max-w-full", className)}>
+            <button onClick={handleClose} className="self-end">
+              <ModalClosed />
+            </button>
+            <div className="flex flex-col text-center items-center gap-4 px-3">
+                {children}
+            </div>
+        </div>
+    </div>
+  )
+}
