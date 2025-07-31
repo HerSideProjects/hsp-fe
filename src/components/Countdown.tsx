66ba58cb8b5d29a87df3fc3f7444986987f53fb3
pershyfon <sabrina.aviana@ui.ac.id>
2025-07-31 12:48:24 +0700

    feat: countdown end

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -11,6 +11,10 @@ const getTimeLeft = () => {
   const now = new Date();
   const difference = targetDate.getTime() - now.getTime();
 
+  if (difference <= 0) {
+    return { days: 0, hours: 0, minutes: 0, seconds: 0 };
+  }
+
   const days = Math.floor(difference / (1000 * 60 * 60 * 24));
   const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
   const minutes = Math.floor((difference / 1000 / 60) % 60);
